4f478cb6bfc8c98c0241737fb145fd0207a51478
Noam Shmueli <noamsh@il.ibm.com>
2012-04-30 06:29:14 +0000

    Add string utils to this project

--- LightingSamples/UniversAALized/LightServer/gen/org/universaal/nativeandroid/lightserver/R.java
+++ LightingSamples/UniversAALized/LightServer/gen/org/universaal/nativeandroid/lightserver/R.java
@@ -181,6 +181,7 @@ public final class R {
     }
     public static final class raw {
         public static final int lighting_service_profile_grounding=0x7f040000;
+        public static final int lighting_service_profile_grounding_backup=0x7f040001;
     }
     public static final class string {
         public static final int app_name=0x7f060001;
