c15cec3193743a00dd82d58048b617745b20be08
Noam Shmueli <noamsh@il.ibm.com>
2012-05-16 08:15:59 +0000

    1. Update the ServiceGrounding xml to fit the Service Registration 2. Change part of the extras names in the LightingServer app

--- LightingSamples/UniversAALized/LightServer/src/org/universaal/nativeandroid/lightserver/service/LightService.java
+++ LightingSamples/UniversAALized/LightServer/src/org/universaal/nativeandroid/lightserver/service/LightService.java
@@ -4,6 +4,7 @@ import org.universaal.nativeandroid.lightserver.model.LightServerModel;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * 
@@ -18,6 +19,8 @@ public class LightService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent pIntent) {
+		Log.i(LightService.class.getCanonicalName(), "Got intent [" + pIntent.getAction() + "]");
+		
 		// Forward the message to the model
 		LightServerModel.getInstance(this).handleIntent(pIntent);
 	}
