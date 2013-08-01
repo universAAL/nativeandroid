d9479956542f8a0fc5ffb357bbb606c76b9f5dde
Alvaro Fides <alfiva@itaca.upv.es>
2013-08-01 12:31:31 +0000

    ignore list

--- mw.container.android/gen/org/universAAL/middleware/container/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.universAAL.middleware.container.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
